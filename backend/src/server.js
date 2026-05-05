a0a8632d7cb6920d555c8301f4aa1239cc59c1ba
simsonson <simsonson706@gmail.com>
2026-05-05 15:40:16 +0530

    Uncomment dotenv and express imports
    Uncomment dotenv and express imports in server.js.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
-/*const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const app = require('./app');
 
@@ -12,9 +12,9 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});*/
+});
 
-import express from "express";
+/*import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
@@ -33,3 +33,4 @@ app.use(cors({
 }));
 
 app.use(express.json());
+*/
